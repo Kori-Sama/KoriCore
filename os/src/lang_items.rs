9271692e408366444164f4246d61a5650602c570
Kori Sama <korisamade@gmail.com>
2025-05-12 10:24:18 +0800

    hello world

---
+++ os/src/lang_items.rs
@@ -0,0 +1,17 @@
+use crate::sbi::shutdown;
+use core::panic::PanicInfo;
+
+#[panic_handler]
+pub fn panic(info: &PanicInfo) -> ! {
+    if let Some(location) = info.location() {
+        println!(
+            "Panicked at {}:{} {}",
+            location.file(),
+            location.line(),
+            info.message()
+        );
+    } else {
+        println!("Panicked: {}", info.message());
+    }
+    shutdown(true)
+}
